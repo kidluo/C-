16a0ff8e90f5b2b2fce7f79dd785cbd84e79815b
罗明康 <luomingkang@luomingkangdemacbook-air.local>
2017-03-12 09:18:18 +0800

    Initial Commit

---
+++ C语言第一次使用/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C语言第一次使用
+//
+//  Created by 罗明康 on 2017/3/12.
+//  Copyright © 2017年 罗明康. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
